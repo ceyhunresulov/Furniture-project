4aab8b10cd7988e6eef339c35eac86e5a6974653
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2022-07-30 23:52:56 +0400

    slider

--- child.js
+++ child.js
@@ -1,8 +1,16 @@
-const chilSlides = document.querySelector(".child__slides");
+const childSlides = document.querySelector(".child__slides");
 const angleLeft = document.querySelector(".child__angle--left");
 const angleRight = document.querySelector(".child__angle--right");
+const left = (childSlides.style.left = 0);
 
 angleLeft.addEventListener("click", () => {
+  console.log(left);
+  if (left > 0) {
+    left = `${position}px`;
+  }
+});
+angleRight.addEventListener("click", () => {
   console.log("salam");
-  chilSlides.style.left = -1200 + "px";
+  position = position + 1215;
+  childSlides.style.left = `${position}px`;
 });
