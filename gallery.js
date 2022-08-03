62235db314dea15bd15e7d4175ac6baf42524999
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2022-08-03 20:18:41 +0400

    gallery hissesinin hundurluyunu dinamiklesdirdim

--- gallery.js
+++ gallery.js
@@ -1,6 +1,8 @@
 const expandIcon = document.getElementById("expand");
 const bigImage = document.querySelector(".big__img");
 const backIcon = document.querySelector(".big__img--reset");
+const gallery = document.getElementById("gallery__grid");
+const galleryImg = document.querySelectorAll("#gallery__grid li");
 expandIcon.addEventListener("click", () => {
   bigImage.style.visibility = "visible";
   bigImage.style.opacity = 1;
@@ -9,3 +11,20 @@ backIcon.addEventListener("click", () => {
   bigImage.style.visibility = "hidden";
   bigImage.style.opacity = 0;
 });
+function getGalleryHeight() {
+  let elMargin = getComputedStyle(galleryImg[0]).marginBottom;
+  let galleryHeight;
+  let width = window.innerWidth;
+  if (width > 1198) {
+    galleryHeight =
+      Math.ceil(galleryImg.length / 4) * galleryImg[0].clientHeight + elMargin;
+  } else if (1198 > width > 720) {
+    galleryHeight = Math.ceil(galleryImg.length / 3) + elMargin;
+  } else if (720 > width > 500) {
+    galleryHeight = Math.ceil(galleryImg.length / 2) + elMargin;
+  } else if (width < 500) {
+    galleryHeight = Math.ceil(galleryImg.length / 1) + elMargin;
+  }
+  gallery.style.height = galleryHeight + "px";
+}
+getGalleryHeight();
--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-// header
-const headerColor1 = "rgba(30,29,28,.5)";
-const headerColor2 = "#302928";
-const header = document.querySelector("header");
-const dropdownHome = document.querySelector(".catalog__dropdown--home");
-const dropdown = document.querySelector(".catalog__dropdown");
-
-window.addEventListener("scroll", getScroll);
-
-function getScroll() {
-  if (window.scrollY > 250) {
-    changeHeaderStyle();
-  } else {
-    header.style.background = headerColor1;
-    header.style.height = 100 + "px";
-    dropdown.style.top = 60 + "px";
-    dropdownHome.style.background = headerColor1;
-  }
-}
-function changeHeaderStyle() {
-  header.style.background = headerColor2;
-  header.style.height = 80 + "px";
-  dropdown.style.top = 40 + "px";
-  dropdownHome.style.background = headerColor2;
-}
+import * as headerChange from "./header.js";
+import * as headerColorChange from "./headerColor.js";
+import * as childSlider from "./childSlider.js";
